7f8c221f34a9f4e4428276601af2ef1576fe35f9
alexis <alexis.saransig@jobsity.com>
2017-03-23 03:18:45 -0500

    Prepares the main layout and basic style

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,400,700" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div class="container">
+    <h1 class="center">Welcome to <strong>Pico y Placa App</strong></h1>
+    <div class="content">
+        @yield('content')
+    </div>
+</div>
+</body>
+</html>
