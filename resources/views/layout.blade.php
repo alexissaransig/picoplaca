85d64712da08f9f81f146490f9d546b15d7602a0
alexis <alexis.saransig@jobsity.com>
2017-03-24 23:48:15 -0500

    Updating pages layout and welcome page.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,400,700" rel="stylesheet">
+    <title>Pico y Placa App</title>
     <link href="/css/app.css" rel="stylesheet" type="text/css">
-
 </head>
 <body>
 <div class="container">
-    <h1 class="center">Welcome to <strong>Pico y Placa App</strong></h1>
-    <div class="content">
+    <h1 class="text-center">Welcome to <strong>Pico y Placa App</strong></h1>
+    <hr>
+    <div class="row">
         @yield('content')
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 @extends('layout')
 
 @section('content')
-    <div class="center">
+    <div class="text-center">
         <p>Please, select an option</p>
         <div class="wrapper">
-            <a href="{{ action("ScheduleController@schedule") }}" class="button">See schedule</a>
-            <a href="{{ action("ScheduleController@schedule") }}" class="button">See schedule</a>
+            <a href="{{ action("PicoPlacaController@picoplaca") }}" class="btn btn-default">Check my license</a>
+            <a href="{{ action("ScheduleController@schedule") }}" class="btn btn-default">See schedule</a>
         </div>
     </div>
 @stop
